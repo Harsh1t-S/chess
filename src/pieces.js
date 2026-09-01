295e28e1171940206bb33ce5853e890a8e308efb
Harshit <harshitkrsharma07@gmail.com>
2026-09-01 13:18:49 +0530

    fix: use canonical Chess.com Neo piece asset path

--- src/pieces.js
+++ src/pieces.js
@@ -1,8 +1,8 @@
-const CLASSIC_BASE='https://images.chesscomfiles.com/chess-themes/pieces/classic/300'
+const NEO_BASE='https://www.chess.com/chess-themes/pieces/neo/300'
 
 export function pieceSvg(code,extra=''){
   if(!code||code.length<2)return ''
   const color=code[0],type=code[1]
-  const src=`${CLASSIC_BASE}/${color}${type}.png`
+  const src=`${NEO_BASE}/${color}${type}.png`
   return `<img class="chess-piece ${color==='w'?'piece-white':'piece-black'} ${extra}" data-piece="${code}" src="${src}" alt="" draggable="false" aria-hidden="true" style="width:100%;height:100%;object-fit:contain;display:block;user-select:none;-webkit-user-drag:none"/>`
 }
