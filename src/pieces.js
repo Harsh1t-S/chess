8b650470a23d17c1c72a70eec6649b3a410ea46a
Harshit <harshitkrsharma07@gmail.com>
2026-09-01 13:14:12 +0530

    fix: use Chess.com Classic piece assets

--- src/pieces.js
+++ src/pieces.js
@@ -1,8 +1,8 @@
-const NEO_BASE='https://assets-themes.chess.com/image/ejgfv/300'
+const CLASSIC_BASE='https://images.chesscomfiles.com/chess-themes/pieces/classic/300'
 
 export function pieceSvg(code,extra=''){
   if(!code||code.length<2)return ''
   const color=code[0],type=code[1]
-  const src=`${NEO_BASE}/${color}${type}.png`
+  const src=`${CLASSIC_BASE}/${color}${type}.png`
   return `<img class="chess-piece ${color==='w'?'piece-white':'piece-black'} ${extra}" data-piece="${code}" src="${src}" alt="" draggable="false" aria-hidden="true" style="width:100%;height:100%;object-fit:contain;display:block;user-select:none;-webkit-user-drag:none"/>`
 }
